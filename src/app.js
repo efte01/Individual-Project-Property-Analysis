5809bbc1f70d8bc90791f19ad2bba44d2ae4c9a6
efte01 <61286409+efte01@users.noreply.github.com>
2022-05-04 00:30:39 +0100

    Created HTML Table and CSS using Bootswatch

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ const BASE_URL = "http://localhost:8000/" // Base url for server, which we add o
 
 form.addEventListener('submit', formSubmitted); // Listener to the submit button, anytime its clicked run function
 
-// Create a HTML List, so we can dynamically add 10 elements
-var list_div = document.createElement("div")
-list_div.id = "names";
-document.getElementsByTagName('body')[0].appendChild(list_div);
-const feedDisplay = document.querySelector('#names')
+// Create a table, with a 'thead', and blank 'tbody'. the loop will then create a 'tr' and then all the 'th' + 'td' attributes
+var table_class = document.createElement("table")
+table_class.className = "table table-hover";
+table_class.id = "table_id"
+table_class.style.display = "none"
+document.getElementsByTagName('body')[0].appendChild(table_class);
+const thead_html = `<thead><tr class="table-active"><th scope="col">NAME</th><th scope="col">ADDRESS</th><th scope="col">PUBLICATION DATE</th><th scope="col">DEADLINE DATE</th><th scope="col">URL</th><th scope="col">EXECUTOR NAME</th><th scope="col">EXECUTOR ADDRESS</th></tr></thead>`
+const table_class_id = document.querySelector('#table_id')
+table_class_id.insertAdjacentHTML("beforeend",thead_html)
+var tbody_class = document.createElement("tbody")
+tbody_class.id = "names";
+document.getElementsByTagName('table')[0].appendChild(tbody_class);
+const names_id = document.querySelector('#names')
 
 function formSubmitted(event) {
     event.preventDefault()
@@ -25,14 +33,15 @@ function getSearchResults(searchTerm) {
 }// Fetch the array from the localhost:8000/custom url
 
 function showResults(results) {
-    while (list_div.hasChildNodes()) {
-        list_div.removeChild(list_div.firstChild);
+    table_class.style.display = "block"
+    while (tbody_class.hasChildNodes()) {
+        tbody_class.removeChild(tbody_class.firstChild);
     } // If divs exist already, remove all of them, so we can input new ones
     results.forEach(names_array => {
-        const title = `<div><p>` + names_array.name + `</p><p>` + names_array.address + `</p><p>` + names_array.publication_date + `</p><p>`
-            + names_array.deadline_date + `</p><p>` + names_array.link + `</p><p>` + names_array.executor_name + `</p><p>` + names_array.executor_address + `</p></div>`
+        const title = `<tr><th scope="row">` + names_array.name + `</th><td>` + names_array.address + `</td><td>` + names_array.publication_date + `</td><td>`
+            + names_array.deadline_date + `</td><td>` + names_array.link + `</td><td>` + names_array.executor_name + `</td><td>` + names_array.executor_address + `</td></tr>`
 
-        feedDisplay.insertAdjacentHTML("beforeend",title)
+        names_id.insertAdjacentHTML("beforeend",title)
     }) // For each item in array, create a HTML element and append
 }//
 
