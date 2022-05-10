3427e8c21bca4df938b75607a45af1a1d31c086c
efte01 <61286409+efte01@users.noreply.github.com>
2022-05-10 17:58:16 +0100

    Problem: wanted to update chart dynamilcally based on input
    solution: chart has to be created in the app.js file instead of html, then chart.update with the new values everytime.
    
    Outcome: chart shows 3 lines, faded based on old ones.
    
    next: working on css styling to make it look better.

--- src/app_one.js
+++ src/app_one.js
@@ -40,10 +40,57 @@ other_input.addEventListener('change', listen_for_change)
 monthly_rent_input.addEventListener('change', listen_for_change)
 annual_rent_input.addEventListener('change', listen_for_change)
 
+// Create a Blank graph with default values
+var ctx = document.getElementById('myChart');
+var xValues = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29];
+var yValues = [-62700, -58770, -54840, -50910, -46980, -43050, -39120, -35190, -31260, -27330, -23400, -19470, -15540, -11610, -7680, -3750, 180, 4110, 8040, 11970, 15900, 19830, 23760, 27690, 31620, 35550, 39480, 43410, 47340, 51270];
+var html_chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [
+            {
+                fill: false,
+                backgroundColor: "rgba(255,0,0,1)",
+                borderColor: "rgba(255,0,0,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+                borderWidth: 0.5,
+                data: yValues
+            },
+            {
+                fill: false,
+                backgroundColor: "rgba(255,0,0,0.5)",
+                borderColor: "rgba(255,0,0,0.5)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+                data: yValues
+            },
+            {
+                fill: false,
+                backgroundColor: "rgba(255,0,0,0.2)",
+                borderColor: "rgba(255,0,0,0.2)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+                data: yValues
+            }
+        ]
+    },
+    options: {
+        legend: {display: false},
+        scales: {
+            yAxes: [{ticks: {min: -62700, max:62700}}],
+        },
+    }
+});
+
 function listen_for_change(event) {
     event.preventDefault()
 
-    insert_var_data()
+    const var_one = create_results()
+    const var_two = create_data(var_one)
+
+    addData(html_chart,var_two)
+    updateRange(html_chart)
+    updateLineColors(html_chart)
+    removeData(html_chart)
 }
 
 function total_initial_cost() {
@@ -51,18 +98,16 @@ function total_initial_cost() {
         parseInt(survey_input.value) + parseInt(conveyancing_input.value) + parseInt(stamp_duty_input.value) +
         parseInt(refurbishment_input.value) + parseInt(void_costs_input.value))*-1
 }
-
 function annual_cost() {
     return (parseInt(mortgage_repayments_input.value) + parseInt(building_insurance_input.value) +
         parseInt(contents_insurance_input.value) + parseInt(maintenance_costs_input.value) + parseInt(agent_fees_input.value) +
         parseInt(ground_rent_input.value) + parseInt(service_charge_input.value) + parseInt(gas_safety_certificate_input.value) + parseInt(other_input.value))*-1
 }
-
 function total_income() {
     return parseInt(annual_rent_input.value)
 }
 
-function create_array() {
+function create_results() {
     var cumalitive_array = []
     while (cumalitive_array.length > 0) {
         cumalitive_array.pop()
@@ -73,55 +118,55 @@ function create_array() {
     }
     return cumalitive_array
 }
-
-function insert_var_data() {
-    try{
-        var xValues = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29];
-        const yValues = create_array()
-        var myChart = new Chart(document.getElementById("myChart"), {
-            type: "line",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "rgba(0,0,255,1.0)",
-                    borderColor: "rgba(0,0,255,0.1)",
-                    data: yValues
-                }]
-            },
-            options: {
-                legend: {display: true},
-                scales: {
-                    yAxes: [{ticks: {min: 6, max:16}}],
-                }
-            }
-        })
-
-        myChart.update()
-        console.log("updating chart")
-    }catch (err)
-    {
-        console.log(err)
+function create_data(results) {
+    return {
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(255,0,0,100)",
+        borderColor: "rgba(255,0,0,100)",
+        data: results
     }
 }
+function addData(chart, data) {
+    chart.data.datasets.unshift(data);
+    chart.update();
+}
+function removeData(chart) {
+    chart.data.datasets.pop()
+    chart.update();
+}
+function updateRange(chart) {
+    let largest_y_range = 0 // Settings a suitable y range
+    for (let i = 0; i < 3; i++) {
+        if (chart.data.datasets[i].data.length !== 0) {
+            if (Math.min.apply(null, chart.data.datasets[i].data) * -1 > largest_y_range) {
+                largest_y_range = Math.min.apply(null, chart.data.datasets[i].data) * -1
+            }
+            if (Math.max.apply(null, chart.data.datasets[i].data) > largest_y_range) {
+                largest_y_range = Math.max.apply(null, chart.data.datasets[i].data)
+            }
+        }
+    }
 
-function formSubmitted(event) {
-    event.preventDefault()
+    chart.options = {
+        legend: {display: false},
+        scales: {
+            yAxes: [{ticks: {min: (largest_y_range * -1), max: largest_y_range}}],
+        }
+    }
+    chart.update();
+}
+function updateLineColors(chart) {
+    chart.data.datasets[0].backgroundColor = "rgba(255,0,0,1)"
+    chart.data.datasets[0].borderColor = "rgba(255,0,0,1)"
+    chart.data.datasets[0].pointBackgroundColor= "rgba(255,255,255,1)"
 
-    getSearchResults(searchTerm)
-        .then(showResults);
-}// Get input value and push through to next function
+    chart.data.datasets[1].backgroundColor = "rgba(255,0,0,0.5)"
+    chart.data.datasets[1].borderColor = "rgba(255,0,0,0.5)"
+    chart.data.datasets[1].pointBackgroundColor= "rgba(255,255,255,0.5)"
 
-function getSearchResults(searchTerm) {
-    const searchTerm2 = searchTerm.replace(/ /g, '+')+"4"; // Replace all spaces in postcode with +
-    return fetch(`${BASE_URL}${searchTerm2}`)
-        .then(res => res.json());
-}// Fetch the array from the localhost:8000/custom url
-
-function showResults(results) {
-
-}//
-
-// Reads from http://localhost:8000/results, then converts it to a HTML format
-
+    chart.data.datasets[2].backgroundColor = "rgba(255,0,0,0.2)"
+    chart.data.datasets[2].borderColor = "rgba(255,0,0,0.2)"
+    chart.data.datasets[2].pointBackgroundColor= "rgba(255,255,255,0.2)"
+    chart.update();
+}
