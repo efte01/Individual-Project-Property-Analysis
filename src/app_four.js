e443c6e54dc46da54160944f54e0aa3d259a1a19
efte01 <61286409+efte01@users.noreply.github.com>
2022-05-15 00:37:03 +0100

    Working on regex

--- src/app_four.js
+++ src/app_four.js
@@ -25,12 +25,33 @@ tbody_class.id = "names";
 document.getElementsByTagName('table')[0].appendChild(tbody_class);
 const names_id = document.querySelector('#names')
 
+const submit = document.getElementById('submit')
+const input = document.getElementById('dropdown')
+const result_html = document.createElement("div");
+result_html.style = "margin: 0 0 20px 0"
+result_html.style.display = "none"
+form.insertBefore(result_html,submit)
+
 function formSubmitted(event) {
     event.preventDefault()
 
     const searchTerm = searchInput.value;
-    getSearchResults(searchTerm)
-        .then(showResults);
+    const regex = /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/; //Reused code
+
+    if (searchTerm.match(regex) !== null) {
+        getSearchResults(searchTerm)
+            .then(showResults);
+
+        input.className = "form-control is-valid";
+        result_html.style.display = "block"
+        result_html.className = "valid-feedback"
+        result_html.innerText = "Success!"
+    }else {
+        input.className = "form-control is-invalid";
+        result_html.style.display = "block"
+        result_html.className = "invalid-feedback"
+        result_html.innerText = "Postcode is invalid. Try again?"
+    }
 }// Get input value and push through to next function
 
 function getSearchResults(searchTerm) {
@@ -44,12 +65,19 @@ function showResults(results) {
     while (tbody_class.hasChildNodes()) {
         tbody_class.removeChild(tbody_class.firstChild);
     } // If divs exist already, remove all of them, so we can input new ones
+
     results.forEach(array => {
         const title = `<tr><th scope="row">` + array.address + `</th><td>` + array.address_type + `</td><td>`
             + array.address_price + `</td><td>` + array.address_date + `</td></tr>`
-
         names_id.insertAdjacentHTML("beforeend",title)
     }) // For each item in array, create a HTML element and append
+
+    if (!tbody_class.hasChildNodes()) {
+        input.className = "form-control is-invalid";
+        result_html.style.display = "block"
+        result_html.className = "invalid-feedback"
+        result_html.innerText = "No results found, with this Postcode."
+    }
 }//
 
 // Reads from http://localhost:8000/results, then converts it to a HTML format
